88f3a71f4c25c24c50e32294054181d805bec067
Thabane Ndaba <skinnybonafide@yahoo.com>
2016-10-24 11:03:52 +0200

    Add Medical History

---
+++ app/src/main/java/com/cogent/doconnect/mobileapp/MedicalHistory.java
@@ -0,0 +1,314 @@
+package com.cogent.doconnect.mobileapp;
+
+/**
+ * Created by Thabane.N on 2016/10/24.
+ */
+
+public class MedicalHistory {
+
+    public MedicalHistory()
+    {
+    }
+
+    public int ID;
+    public String FirstName;
+    public String LastName;
+    public String ID_Number;
+    public String Gender;
+    public String DOB;
+    public String Cell_Number;
+    public String Street_Address;
+    public String Suburb;
+    public String City;
+    public String Country;
+    public int Medical_Aid_ID;
+    public int Doctor_ID;
+    public int User_ID;
+    public String Allergies;
+    public String PreviousIllnesses;
+    public String PreviousMedication;
+    public String RiskFactors;
+    public String SocialHistory;
+    public String FamilyHistory;
+    public String Email;
+    public int Patient_Medical_Aid_ID;
+    public String Scheme_Name;
+    public String Membership_Number;
+    public boolean Status;
+    public String Registration_Date;
+    public String Deregistration_Date;
+    public int Patient_ID;
+    public int Patient_Medical_Aid_Medical_Aid_ID;
+    public String Name;
+
+    public int GetID()
+    {
+        return ID;
+    }
+    public void SetID(int id)
+    {
+        ID = id;
+    }
+    //***************************************************
+    public String GetFirstName()
+    {
+        return FirstName;
+    }
+    public void SetFirstName(String firstName)
+    {
+        FirstName = firstName;
+    }
+    //***************************************************
+    public String GetLastName()
+    {
+        return LastName;
+    }
+    public void SetLastName(String lastName)
+    {
+        LastName = lastName;
+    }
+    //***************************************************
+    public String GetID_Number()
+    {
+        return ID_Number;
+    }
+    public void SetID_Number(String id_Number)
+    {
+        ID_Number = id_Number;
+    }
+    //***************************************************
+    public String GetGender()
+    {
+        return Gender;
+    }
+    public void SetGender(String gender)
+    {
+        Gender = gender;
+    }
+    //***************************************************
+    public String GetDOB()
+    {
+        return DOB;
+    }
+    public void SetDOB(String dob)
+    {
+        DOB = dob;
+    }
+    //***************************************************
+    public String GetCell_Number()
+    {
+        return Cell_Number;
+    }
+    public void SetCell_Number(String cell_Number)
+    {
+        Cell_Number = cell_Number;
+    }
+    //***************************************************
+    public String GetStreet_Address()
+    {
+        return Street_Address;
+    }
+    public void SetStreet_Address(String street_Address)
+    {
+        Street_Address = street_Address;
+    }
+    //***************************************************
+    public String GetSuburb()
+    {
+        return Suburb;
+    }
+    public void SetSuburb(String suburb)
+    {
+        Suburb = suburb;
+    }
+    //***************************************************
+    public String GetCity()
+    {
+        return City;
+    }
+    public void SetCity(String city)
+    {
+        City = city;
+    }
+    //***************************************************
+    public String GetCountry()
+    {
+        return Country;
+    }
+    public void SetCountry(String country)
+    {
+        Country = country;
+    }
+    //***************************************************
+    public int GetMedical_Aid_ID()
+    {
+        return Medical_Aid_ID;
+    }
+    public void SetMedical_Aid_ID(int medical_Aid_ID)
+    {
+        Medical_Aid_ID = medical_Aid_ID;
+    }
+    //***************************************************
+    public int GetDoctor_ID()
+    {
+        return Doctor_ID;
+    }
+    public void SetDoctor_ID(int doctor_ID)
+    {
+        Doctor_ID = doctor_ID;
+    }
+    //***************************************************
+    public int GetUser_ID()
+    {
+        return User_ID;
+    }
+    public void SetUser_ID(int user_ID)
+    {
+        User_ID = user_ID;
+    }
+    //***************************************************
+    public String GetAllergies()
+    {
+        return Allergies;
+    }
+    public void SetAllergies(String allergies)
+    {
+        Allergies = allergies;
+    }
+    //***************************************************
+    public String GetPreviousIllnesses()
+    {
+        return PreviousIllnesses;
+    }
+    public void SetPreviousIllnesses(String previousIllnesses)
+    {
+        PreviousIllnesses = previousIllnesses;
+    }
+    //***************************************************
+    public String GetPreviousMedication()
+    {
+        return PreviousMedication;
+    }
+    public void SetPreviousMedication(String previousMedication)
+    {
+        PreviousMedication = previousMedication;
+    }
+    //***************************************************
+    public String GetRiskFactors()
+    {
+        return RiskFactors;
+    }
+    public void SetRiskFactors(String riskFactors)
+    {
+        RiskFactors = riskFactors;
+    }
+    //***************************************************
+    public String GetSocialHistory()
+    {
+        return SocialHistory;
+    }
+    public void SetSocialHistory(String socialHistory)
+    {
+        SocialHistory = socialHistory;
+    }
+    //***************************************************
+    public String GetEmail()
+    {
+        return Email;
+    }
+    public void SetEmail(String email)
+    {
+        Email = email;
+    }
+    //***************************************************
+    public String GetFamilyHistory()
+    {
+        return FamilyHistory;
+    }
+    public void SetFamilyHistory(String familyHistory)
+    {
+        FamilyHistory = familyHistory;
+    }
+    //***************************************************
+    public int GetPatient_Medical_Aid_ID()
+    {
+        return Patient_Medical_Aid_ID;
+    }
+    public void SetPatient_Medical_Aid_ID(int patient_Medical_Aid_ID)
+    {
+        Patient_Medical_Aid_ID = patient_Medical_Aid_ID;
+    }
+    //***************************************************
+    public String GetScheme_Name()
+    {
+        return Scheme_Name;
+    }
+    public void SetScheme_Name(String scheme_Name)
+    {
+        Scheme_Name = scheme_Name;
+    }
+    //***************************************************
+    public String GetMembership_Number()
+    {
+        return Membership_Number;
+    }
+    public void SetMembership_Number(String membership_Number)
+    {
+        Membership_Number = membership_Number;
+    }
+    //***************************************************
+    public boolean GetStatus()
+    {
+        return Status;
+    }
+    public void SetStatus(boolean status)
+    {
+        Status = status;
+    }
+    //***************************************************
+    public String GetRegistration_Date()
+    {
+        return Registration_Date;
+    }
+    public void SetRegistration_Date(String registration_Date)
+    {
+        Registration_Date = registration_Date;
+    }
+    //***************************************************
+    public String GetDeregistration_Date()
+    {
+        return Deregistration_Date;
+    }
+    public void SetDeregistration_Date(String deregistration_Date)
+    {
+        Deregistration_Date = deregistration_Date;
+    }
+    //***************************************************
+    public String GetName()
+    {
+        return Name;
+    }
+    public void SetName(String name)
+    {
+        Name = name;
+    }
+    //***************************************************
+    public int GetPatient_ID()
+    {
+        return Patient_ID;
+    }
+    public void SetPatient_ID(int patient_ID)
+    {
+        Patient_ID = patient_ID;
+    }
+    //***************************************************
+    public int GetPatient_Medical_Aid_Medical_Aid_ID()
+    {
+        return Patient_Medical_Aid_Medical_Aid_ID;
+    }
+    public void SetPatient_Medical_Aid_Medical_Aid_ID(int patient_Medical_Aid_Medical_Aid_ID)
+    {
+        Patient_Medical_Aid_Medical_Aid_ID = patient_Medical_Aid_Medical_Aid_ID;
+    }
+    //***************************************************
+}
