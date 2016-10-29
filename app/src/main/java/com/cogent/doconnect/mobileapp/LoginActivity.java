732c4c4b7af2851ea498a18db98c86490fa477bc
Tshidiso <tshidi.phaladi@gmail.com>
2016-10-29 20:15:58 +0200

    Added All Activities

--- app/src/main/java/com/cogent/doconnect/mobileapp/LoginActivity.java
+++ app/src/main/java/com/cogent/doconnect/mobileapp/LoginActivity.java
@@ -1,12 +1,21 @@
 package com.cogent.doconnect.mobileapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.Snackbar;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,32 +23,42 @@ public class LoginActivity extends AppCompatActivity {
     EditText txtUsername;
     EditText txtPassword;
     Button btnRegister;
+
+    Connection conn;
+    String userName, pass, db, ip;
+    Boolean isSuccess = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        btnLogin = (Button)findViewById(R.id.login);
-        txtUsername = (EditText)findViewById(R.id.userName);
-        txtPassword = (EditText)findViewById(R.id.password);
-        btnRegister = (Button)findViewById(R.id.register);
+        btnLogin = (Button) findViewById(R.id.login);
+        txtUsername = (EditText) findViewById(R.id.userName);
+        txtPassword = (EditText) findViewById(R.id.password);
+        btnRegister = (Button) findViewById(R.id.register);
 
-        btnLogin.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                if (txtUsername.getText().equals("") || txtPassword.getText().equals(""))
-                {
+        ip = "doconnect.database.windows.net";
+        db = "DoConnect";
+        userName = "teamCogent";
+        pass = "DoConnect1";
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (txtUsername.getText().equals("") || txtPassword.getText().equals("")) {
                     Snackbar.make(v, "Please username and password", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
                 else
                 {
-                    //WS call
+
                 }
             }
         });
 
-        btnRegister.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 Intent register = new Intent("android.intent.action.SIGNUPACTIVITY");
                 startActivity(register);
             }
